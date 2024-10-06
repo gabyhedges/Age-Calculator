56fe6a2a1a99d94036bf93ce0ef27915fb1baad8
Gaby hedges <79882381+gabyhedges@users.noreply.github.com>
2024-10-06 12:27:37 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const dobInput = document.getElementById('dob');
+const calculateBtn = document.getElementById('calculate-btn');
+const resultDiv = document.getElementById('result');
+
+calculateBtn.addEventListener('click', function() {
+    const dob = new Date(dobInput.value);
+    const ageInMs = Date.now() - dob.getTime(); // Call Date.now() as a function
+    const ageDate = new Date(ageInMs);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    resultDiv.innerHTML = `Your age is ${age} years. `;
+});
